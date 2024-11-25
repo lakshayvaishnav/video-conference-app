56570b7f9994901fa81bfef315fb4f57dc47b3a8
Lakshay Vaishnav <thelakshayvaishnav@gmail.com>
2024-11-26 01:08:16 +0530

    added sidebar and navbar

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,4 @@
+export default function () {
+    return <div>this is the home page.</div>;
+  }
+  
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+const Rootlayout = ({ children }: { children: ReactNode }) => {
+  return <main>{children}</main>;
+};
+
+export default Rootlayout;
